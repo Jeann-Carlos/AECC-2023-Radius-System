2f92d63f191fe5c144248a27fc95281d117b181b
jeann <jeann.hernandez@upr.edu>
2023-10-06 15:20:46 -0400

    Reorganizacion del codigo, faltaria anadir una forma de salir del programa sin causar problemas.

--- AeccPiholeCursor.py
+++ AeccPiholeCursor.py
@@ -5,24 +5,24 @@
 Author: Jeann C. Hernandez Franco
 Co-Author: Luis F. Velazquez
 Date: October 5, 2023
-Last Updated: ---------
+Last Updated: October 6, 2023
 '''
-
 
 import os
 import sqlite3
 import time
 import schedule
+import asyncio
 
+#Global Variables
+schedule_timer=.10
 
 def StartService():
+
     try:
-        # Start by connecting to Db
-        cursor,conn = ConnectToDb()
 
         # Schedule the job to run every 5 minutes
-        schedule.every(.10).minutes.do(CheckUsers,cursor=cursor,conn=conn)
-        # schedule.every(1).minutes.do(CloseProgram)
+        schedule.every(schedule_timer).minutes.do(DriverProgram)
 
         # Keep the service running indefinitely
         while True:
@@ -36,10 +36,12 @@ def StartService():
     except Exception as e:
         print('Unhandled Exception caught. Check error below for details:')
         print(e)
-        return -1 # return -1 as a signal error
+        return -1  # return -1 as a signal error
+
 
 def ConnectToDb():
 
+    # Name of DB
     database_file = "gravity.db"
 
     # Check if the database file exists
@@ -54,16 +56,24 @@ def ConnectToDb():
     cursor = conn.cursor()
 
     # Return the cursor and connection
-    return cursor,conn
+    return cursor, conn
 
 
-def DiscconectFromDb(cursor,conn):
-        cursor.close()
-        conn.close()
-        print('Successfully disconnected...Have a nice day!')
+def DiscconectFromDb(cursor, conn):
+    cursor.close()
+    conn.close()
+    print('Successfully disconnected...Have a nice day!')
 
-def SaveTable():
-    pass
+
+def UpdateDb(conn):
+    try:
+        conn.commit()
+    except Exception as e:
+        print('Rolling back any changes...')
+        conn.rollback()
+        return e
+
+
 
 def GetTable():
     file_path = "Newly_Registered_Members.txt"
@@ -80,33 +90,49 @@ def GetTable():
     return open(file_path, 'r')
 
 
-def CheckUsers(cursor,conn):
+def DriverProgram():
+
+    # Start by connecting to Db
+    cursor, conn = ConnectToDb()
+
+    # Get new users that paid
     newly_registered_members = [L.strip() for L in GetTable()]
-    CompareTables(cursor,newly_registered_members)
+
+    # Compare users that paid with the registered tables in gravity db
+    CompareTables(cursor, newly_registered_members)
+
+    # Commit changes
+    UpdateDb(conn)
+
+    # Disconnect from DB
+    DiscconectFromDb(cursor, conn)
+
+    # Notify user that is safe to close program now
 
 
 
-def CompareTables(cursor,newly_registered_members):
+def CompareTables(cursor, newly_registered_members):
+
+    # Select all registered members
     cursor.execute('select ip from "client_by_group" join client where client_id=client.id and group_id=1')
     already_registered_members_list = cursor.fetchall()
+
     for new_member in newly_registered_members:
         if new_member in [ip[0] for ip in already_registered_members_list]:
             print(f"User: {new_member} already registered as a member...Ignoring")
         else:
             cursor.execute(f'select * from client where client.ip="{new_member}"')
             member_data = cursor.fetchall()
-            # check if user already exists in Pihole if pihole doesnt have him there's a problem
+
+            # Check if user already exists in Pihole, if pihole doesn't have him there's a problem
             if member_data == []:
                 pass
             else:
-                # add user to registered members
-                 cursor.execute(f'insert into client_by_group (client_id, group_id) values ({new_member[0][0]},1')
-                 cursor.execute(f"DELETE FROM client_by_group WHERE client_id = {new_member[0][0]} AND group_id = 0")
+                # Add user to registered members
+                print(f"Inserting user with ip: {new_member} at the Db")
+                cursor.execute(f"INSERT INTO client_by_group(client_id, group_id) VALUES ({int(member_data[0][0])}, 1)")
+                cursor.execute(f"DELETE FROM client_by_group WHERE client_id = {member_data[0][0]} AND group_id = 0")
 
-class CloseProgram:
-    # user_input = input("Press 'q' to quit: ")
-    #if user_input.lower() == "q":
-    pass
 
 exit_value = StartService()
 exit(exit_value)
